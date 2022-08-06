8a02973f6faba081da62df429a07723295d3eb91
tuyojr <olutuyod@gmail.com>
2022-08-06 12:15:53 +0100

    classes and objects

---
+++ Class and Objects II.py
@@ -0,0 +1,7 @@
+
+class pizza1:
+    def __int__(self,type,price,size):
+        self.type = type
+        self.price = price
+        self.size = size
+
---
+++ Class and Objects.py
@@ -0,0 +1,7 @@
+
+from Impt import pizza1
+
+pizzaV = pizza1("Vegetarian",5,"Small")
+
+print(pizzaV.type)
+
