20d4402338427ece354609076a8b6667314c11fa
tuyojr <olutuyod@gmail.com>
2022-08-06 12:19:09 +0100

    dictionary

---
+++ Dictionary.py
@@ -0,0 +1,16 @@
+
+colors = { "B" : "Blue",
+           "R" : "Red",
+           "O" : "Orange"   
+}
+print(colors.get("H","Stop"))
+
+
+Employees ={ 152 : "Blade",
+             233 : "Hulk",
+             254 : "John",
+             277 : "Saxe",
+             347 : "Xavier"
+    
+}
+print(Employees.get(348,"There is no employee with this tag."))
